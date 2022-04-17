de81109e68ec387d78e28640845cbdae427d85aa
Tigistgudeta2009 <tigistgudeta2021@gamil.com>
2022-04-17 08:52:05 -0700

    commit message

--- src/pl/vistula/exercise7/Main.java
+++ src/pl/vistula/exercise7/Main.java
@@ -5,8 +5,19 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args)  throws IOException {
         Staff bb = new Staff();
-        bb.initialize();
-        bb.Gudeta568171();
+        //bb.initialize();
+        //bb.Gudeta568171();
+
+        double salary = bb.salaryToPay_Gudeta_56817(150, 30);
+        System.out.println("The employee received a salary of"+ salary);
+
+        double salaryForOvertime=bb.salaryForOvertime_gudeta_56817(12,30);
+        System.out.println("The employees received a salary for overtime in the amount of" + salaryForOvertime);
+
+        int bonus=Salary.bonus_Gudeta_56817(5);
+        System.out.println("The employees received a bonus "+ bonus);
+
+
 
     }
 }
--- src/pl/vistula/exercise7/Staff.java
+++ src/pl/vistula/exercise7/Staff.java
@@ -4,9 +4,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Staff extends Person{
+public class Staff extends Person implements Salary{
     String education;
     String position;
+
+    double salary;
 
     public  void  initialize() throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,10 +17,21 @@ public class Staff extends Person{
         education = br.readLine();
         System.out.println("Enter your position");
         position = br.readLine();
+
     }
     public void Gudeta568171(){
         Gudeta56817();
         System.out.println("Education :" + education);
         System.out.println("Position :" + position);
     }
+
+    @Override
+
+    public double salaryToPay_Gudeta_56817(double hours, double rate) {
+        return hours * rate;
+    }
+    public double salaryForOvertime_gudeta_56817(double overtimeHours, double rate){
+        return Salary.super.salaryForOvertime_Gudeta_56817(overtimeHours,rate);
+    }
+
 }
